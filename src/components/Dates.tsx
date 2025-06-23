7946aac63e2671c475e0c016b4fc8c814b1db7ad
hxunz <guswl2843@gmail.com>
2025-06-23 15:33:36 +0100

    Open popup when a specific date is clicked

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 interface DatesProps {
   year: number;
   month: number;
@@ -8,13 +10,31 @@ const Dates: React.FC<DatesProps> = ({ year, month }) => {
 
   const dates = Array.from({ length: lastDate }, (v, i) => i + 1);
 
+  const handleClickOpenDialog = () => {
+    alert('open');
+  };
+
   return (
-    <div className="grid grid-cols-7 gap-4 w-full p-4">
-      {dates.map((day) => (
-        <button key={day} className='text-center inline-block text-red cursor-pointer'>{day}</button>
-      ))}
-      <div className='background-color' />
-    </div>
+    <>
+      <div className="grid grid-cols-7 gap-4 w-full p-4">
+        {dates.map((day) => (
+          <button
+            key={day}
+            className='text-center inline-block text-red cursor-pointer'
+            onClick={handleClickOpenDialog}
+          >
+            {day}
+          </button>
+        ))}
+        <div className='background-color' />
+      </div>
+      <dialog>
+        <button>close</button>
+        <div>
+          Record your alcohol !
+        </div>
+      </dialog>
+    </>
   );
 }
 
